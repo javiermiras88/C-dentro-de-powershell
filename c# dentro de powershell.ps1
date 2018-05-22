1c7054f0566e3cb9d66fd14e43ca468dcd6504e0
javiermiras88 <javiermiras88@hotmail.com>
2018-05-22 21:41:18 +0200

    .

---
+++ c# dentro de powershell.ps1
@@ -0,0 +1,51 @@
+﻿cls
+
+
+Add-Type -TypeDefinition $codigo -Language CSharp
+
+
+
+$codigo=@"
+
+
+
+using System;
+
+
+
+
+namespace javi
+
+{
+
+
+
+
+    public static class start{
+    
+        public static void Main()
+        
+        {
+        
+
+            Console.WriteLine("hola");
+        
+        }
+        
+    
+    
+    
+    }
+
+
+
+
+}
+
+
+
+
+"@
+
+
+[javi.start]::Main()
